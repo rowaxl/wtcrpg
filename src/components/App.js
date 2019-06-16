b10a4da86bafb146ca61de368b94f533678275ae
rowaxl <rowaxl0@gmail.com>
2019-06-16 20:24:30 +0900

    add empty header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Toolbar from './templates/Toolbar';
 import SigninPage from './pages/SigninPage';
 
 export default class App extends React.Component {
     render() {
         return (
-            <SigninPage />
+            <BrowserRouter>
+                <Toolbar />
+                <Route exact path="/" component={SigninPage} />
+            </BrowserRouter>
         );
     }
 }
---
+++ src/components/templates/Toolbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MenuHeader } from 'semantic-ui-react'
+
+export default class Toolbar extends React.Component {
+    render() {
+        return (
+            <MenuHeader>This is Toolbar</MenuHeader>
+        )
+    }
+}
