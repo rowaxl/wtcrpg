746eb337f4fbec1b735b087b913171427e3714c9
rowaxl <rowaxl0@gmail.com>
2019-06-18 21:50:42 +0900

    update prototype text input

--- src/components/atoms/TextInput.js
+++ src/components/atoms/TextInput.js
@@ -2,12 +2,39 @@ import React from 'react';
 import { Input } from 'semantic-ui-react';
 
 export default class TextInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if(!this.state.value) return;
+        
+        // TODO: when submit, run actions to post
+        console.log(this.state.value);
+        this.setState({ value: '' });
+    }
 
     render() {
         return (
-            <div>
-                <Input fluid icon='chat' placeholder='Type and press return, if you want.' />
-            </div>
+            <form onSubmit={this.handleSubmit.bind(this)} >
+                <Input
+                    fluid
+                    type='text'
+                    icon='chat'
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                    placeholder='Type and press return, if you want.'
+                />
+            </form>
         )
     }
 }
