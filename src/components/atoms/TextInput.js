1e266744e2cb0bcd160459da51a42c87404da206
rowaxl <rowaxl0@gmail.com>
2019-07-02 21:36:53 +0900

    add rolling dice function

--- src/components/atoms/TextInput.js
+++ src/components/atoms/TextInput.js
@@ -16,10 +16,17 @@ class TextInput extends React.Component {
         this.setState({ value: event.target.value });
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
         const text = this.state.value;
         if(!text) return;
+
+        if (text.indexOf('/dice') > -1) {
+            const diceList = text.split('/dice')[1].split(' ').filter(e => e);
+            console.log(diceList);
+            const result = await this.rollDices(diceList);
+            console.log(result);
+        }
 
         // TODO: this will be DELETED
         const _id = this.props.messages.length + 1;
@@ -27,6 +34,24 @@ class TextInput extends React.Component {
         this.props.sendMessage({id:_id, user:this.props.user, text, created:Date.now()});
         this.setState({ value: '' });
     }
+
+    rollDices = diceList => new Promise((resolve, reject) => {
+        let result = [];
+        diceList.forEach(dice => {
+            const diceNum = dice.split(/[dD]/g)[0];
+            const diceType = dice.split(/[dD]/g)[1];
+
+            let i = 0;
+            while(++i <= diceNum) {
+                // min value must be 1
+                const rolled = Math.floor(Math.random() * parseInt(diceType) + 1);
+                result.push({dice, rolled});
+            }
+        });
+
+        if(result.length === 0) return reject('failed to rolled the dice');
+        resolve(result);
+    })
 
     render() {
         return (
